c92820ce5cfb08c7fedc86e3969965ee7660e914
Arla <febrianusarif19@gmail.com>
2025-01-29 18:06:07 +0700

    update

---
+++ src/js/scrollEffect.js
@@ -0,0 +1,34 @@
+ScrollReveal({
+  reset: true,
+  distance: "60px",
+  duration: 1500,
+  delay: 400,
+});
+// nav
+ScrollReveal().reveal(".container-nav", { delay: 200, origin: "top" });
+
+// hero
+ScrollReveal().reveal(".heroImg", { delay: 200, origin: "left" });
+ScrollReveal().reveal(".desc", { delay: 210, origin: "top" });
+ScrollReveal().reveal(".socialMedia", { delay: 220, origin: "bottom" });
+// about
+ScrollReveal().reveal(".imgSide", { delay: 200, origin: "left" });
+ScrollReveal().reveal(".contentAbout", { delay: 200, origin: "top" });
+// porto
+ScrollReveal().reveal(".h2p", { delay: 400, origin: "left" });
+ScrollReveal().reveal(".card", { delay: 200, origin: "top", easing: "ease-in", interval: 200, scale: ".95" });
+// skill
+ScrollReveal().reveal(".h2My", { delay: 400, origin: "left" });
+ScrollReveal().reveal(".cardSkill", { delay: 200, easing: "ease-in", interval: 200, scale: ".95" });
+// service
+ScrollReveal().reveal(".h2s", { delay: 200, origin: "left" });
+ScrollReveal().reveal(".wd", { delay: 200, origin: "top" });
+ScrollReveal().reveal(".lp", { delay: 200, origin: "bottom" });
+// contact
+ScrollReveal().reveal(".h2c", { delay: 200, origin: "left" });
+ScrollReveal().reveal(".contentC1", { delay: 200, origin: "top" });
+ScrollReveal().reveal(".contentC2", { delay: 200, origin: "bottom" });
+// footer
+ScrollReveal().reveal(".info", { delay: 200, origin: "left" });
+ScrollReveal().reveal(".link", { delay: 200, origin: "bottom" });
+ScrollReveal().reveal(".medsos", { delay: 200, origin: "top" });
